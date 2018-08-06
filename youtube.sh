9b769cef611cf71bbf4a122738b152e8678361df
kboghdady <kirolos@gmail.com>
2018-08-06 13:48:05 -0700

    store links into temp file
    Store the data into templist.txt and then only add the uique list into the backlists.txt

--- youtube.sh
+++ youtube.sh
@@ -9,15 +9,21 @@
 
 
 balckListFile='/etc/pihole/blacklist.txt'
+tempList='/etc/pihole/youtube.txt'
 
 # Get the domain list from hackeragent api 
 # change it to be r[Number]---sn--
 # added to the youtubeFile
 sudo curl 'https://api.hackertarget.com/hostsearch/?q=googlevideo.com' \
-| awk -F, 'NR>1{print $1}'|sed "s/\(^r[[:digit:]]*\)\.\(sn\)/\1---\2-/ ">>$balckListFile
+| awk -F, 'NR>1{print $1}'|sed "s/\(^r[[:digit:]]*\)\.\(sn\)/\1---\2-/ ">>$tempList
 
 
 # collecting the youtube ads website from the pihole logs and added it the youtubeList.txt 
-sudo cat /var/log/pihole*.log |grep 'r[0-9]*-.*.googlevideo'|awk '{print $8}'|sort |uniq >> $balckListFile
+sudo cat /var/log/pihole*.log |grep 'r[0-9]*-.*.googlevideo'|awk '{print $8}'|sort |uniq >> $tempList
+
+# add only the uniqe names to the balcklist file 
+sudo sort $balckListFile $tempList |uniq -u >> $balckListFile
+# delete the templist from its location
+sudo rm $tempList
 
 
