18fc361891c521ae1797fb3e1e9008c27c947e82
Deniz Dogan <denizdogan@users.noreply.github.com>
2019-08-11 21:43:19 +0200

    Fix variable typo
    $balckListFile -> $blackListFile

--- youtube.sh
+++ youtube.sh
@@ -10,25 +10,25 @@ piholeIPV4=$(hostname -I |awk '{print $1}')
 piholeIPV6=$(hostname -I |awk '{print $2}')
 
 
-balckListFile='/etc/pihole/black.list'
+blackListFile='/etc/pihole/black.list'
 blacklist='/etc/pihole/blacklist.txt'
 
 # Get the domain list from hackeragent api 
 # change it to be r[Number]---sn--
 # added to the youtubeFile
 sudo curl 'https://api.hackertarget.com/hostsearch/?q=googlevideo.com' \
-| awk -F, 'NR>1{print $1}'|sed "s/\(^r[[:digit:]]*\)\.\(sn\)/$piholeIPV4 \1---\2-/ ">>$balckListFile
+| awk -F, 'NR>1{print $1}'|sed "s/\(^r[[:digit:]]*\)\.\(sn\)/$piholeIPV4 \1---\2-/ ">>$blackListFile
 
 sudo curl 'https://api.hackertarget.com/hostsearch/?q=googlevideo.com' \
-| awk -F, 'NR>1{print $1}'|sed "s/\(^r[[:digit:]]*\)\.\(sn\)/$piholeIPV6 \1---\2-/ ">>$balckListFile
+| awk -F, 'NR>1{print $1}'|sed "s/\(^r[[:digit:]]*\)\.\(sn\)/$piholeIPV6 \1---\2-/ ">>$blackListFile
 
 sudo curl 'https://api.hackertarget.com/hostsearch/?q=googlevideo.com' \
 | awk -F, 'NR>1{print $1}'|sed "s/\(^r[[:digit:]]*\)\.\(sn\)/\1---\2-/ ">>$blacklist
 
 # collecting the youtube ads website from the pihole logs and added it the blacklist.txt
 #Also, Collect the youtube videos from the Pihole logs 
-sudo cat /var/log/pihole.log* |grep '^r[0-9]*-.*.googlevideo'|grep -v '\-\-\-sn\-n4v7' |awk -v a="$piholeIPV4" '{print a " " $8}'|sort |uniq>> $balckListFile
-sudo cat /var/log/pihole.log* |grep '^r[0-9]*-.*.googlevideo'|grep -v '\-\-\-sn\-n4v7' |awk -v a="$piholeIPV6" '{print a " " $8}'|sort |uniq>> $balckListFile
+sudo cat /var/log/pihole.log* |grep '^r[0-9]*-.*.googlevideo'|grep -v '\-\-\-sn\-n4v7' |awk -v a="$piholeIPV4" '{print a " " $8}'|sort |uniq>> $blackListFile
+sudo cat /var/log/pihole.log* |grep '^r[0-9]*-.*.googlevideo'|grep -v '\-\-\-sn\-n4v7' |awk -v a="$piholeIPV6" '{print a " " $8}'|sort |uniq>> $blackListFile
 sudo cat /var/log/pihole.log* |grep '^r[0-9]*-.*.googlevideo'|grep -v '\-\-\-sn\-n4v7' |awk '{print $8}'|sort |uniq>> $blacklist
 sudo cat /var/log/pihole.log* |awk '{print $6}'|grep '^r[0-9]*-.*.googlevideo'|grep -v '\-\-\-sn\-n4v7' |sort |uniq>> $blacklist
 wait 
@@ -38,7 +38,6 @@ dpkg -l | grep -qw gawk || sudo apt-get install gawk -y
 
 wait 
 # remove the duplicate records in place
-gawk -i inplace '!a[$0]++' $balckListFile
+gawk -i inplace '!a[$0]++' $blackListFile
 wait 
 gawk -i inplace '!a[$0]++' $blacklist
-
