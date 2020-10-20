fc01fbadaf8696b0f13b428b49f30cf8d1bdabfe
Luca Attanasio <blackwiz4rd@gmail.com>
2020-10-20 22:22:54 +0200

    sudo fix for folders removal

--- uninstall.sh
+++ uninstall.sh
@@ -3,7 +3,7 @@ repoDir='/pi/youTube_ads_4_pi-hole'
 # Pihole configuration directory to be removed
 piholeConfigDir='/etc/pihole'
 # Remove directories
-rm -rf $repoDir $piholeConfigDir
+sudo rm -rf $repoDir $piholeConfigDir
 # remove gawk
 sudo apt-get remove gawk -y
 # restard dns
