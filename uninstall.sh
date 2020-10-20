ff2df00d7099c45d010baf634ebfe1c631eee0ce
Luca Attanasio <blackwiz4rd@gmail.com>
2020-10-20 22:28:29 +0200

    fixed wrong folder rm

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,9 @@
 # This need to be chnaged to your actual repo dir on your machine
 repoDir='/pi/youTube_ads_4_pi-hole'
-# Pihole configuration directory to be removed
-piholeConfigDir='/etc/pihole'
 # Remove directories
-sudo rm -rf $repoDir $piholeConfigDir
+sudo rm -rf $repoDir
+# Remove all blacklists
+/usr/bin/sqlite3 /etc/pihole/gravity.db "delete from domainlist where domain like '%googlevideo.com%' "
 # remove gawk
 sudo apt-get remove gawk -y
 # restard dns
